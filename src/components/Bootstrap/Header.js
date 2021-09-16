d1e52b4a75d2fc2ae26dd321843cabbb196ba44b
HemanthK1708 <hemanth.kumarchilamkurthi@gmail.com>
2021-09-16 16:08:18 +0530

    Added Bootstrap Header component

---
+++ src/components/Bootstrap/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav class="Header navbar navbar-light bg-light">
+  <div class="container">
+        <a class="navbar-brand" href="/">
+      <img src="/logo512.png" alt="" width="30" height="24"/>
+    </a>
+  </div>
+</nav>
+    );
+};
+
+export default Header
