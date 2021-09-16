dd60c4d9c522d9e79f3ccf1b3969e3109a40633d
HemanthK1708 <hemanth.kumarchilamkurthi@gmail.com>
2021-09-16 14:35:59 +0530

    Reorganised and removed useless files.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div classname="App"div>
+        <h1>Huuu! </h1>
+        <p>...A messaging application!</p>
+      </div>
+    )
+  }
+}
+
+export default App;
