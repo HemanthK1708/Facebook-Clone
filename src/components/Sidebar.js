bc4897eb507196f30c88b0020c2d85f0123e841d
HemanthK1708 <hemanth.kumarchilamkurthi@gmail.com>
2021-09-17 16:16:01 +0530

    Created a Sidebar componentwith UserMeta

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return <div className="Sidebar col-2">  
+    <div className="UserMeta">
+    <a href="/">
+      <img src="https://s3-us-west-2.amazonaws.com/nv-uploaded-avatars/prod/6b829300-109a-4be3-abf1-f06016e89a83.png" alt="H"/>
+      Hemanth
+      </a>
+    </div>
+    </div>
+}
+
+export default Sidebar
