f085c3554116e7b9d55175563cd7fd541e1391fb
HemanthK1708 <hemanth.kumarchilamkurthi@gmail.com>
2021-09-17 16:02:44 +0530

    Fixed the colours and headers

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import Header from "./Bootstrap/Header";
+//import FBBox from "./FBBox";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header className="Header">Facebook Clone</Header>
+        <div className="container">
+        
+        <div className="row">
+        <div className="col-2">Left</div>
+        <div className="col-7">Middle</div>
+        <div className="col-2">Right</div>
+        </div>
+        </div>
       </div>
     )
   }
--- src/components/Bootstrap/Header.js
+++ src/components/Bootstrap/Header.js
@@ -6,16 +6,24 @@ const Header = ({ dark, children, className }) => {
         <nav
         className={
           `navbar navbar-${dark} bg-${dark}` + (className ? " " + className : "")
-        }
-        >
+        }>
   <div class="container">
       <a class="navbar-brand" href="/">
       <img src="/logo512.png"
        alt={children + " Logo"}
-       className="me-2"
-      />
+       className="me-2"/>
       {children}
     </a>
+    <div className="UserMeta">
+    <a href="/">
+      <img src="https://s3-us-west-2.amazonaws.com/nv-uploaded-avatars/prod/6b829300-109a-4be3-abf1-f06016e89a83.png" alt="H"/>
+      Hemanth
+      </a>
+      <span className="nav-links">
+      <a href="/">Home</a>
+      <a href="/">Create</a>
+      </span>
+    </div>
   </div>
 </nav>
     );
