c0090f7b21ea665cb52e2b781c180ed4cd19c543
HemanthK1708 <hemanth.kumarchilamkurthi@gmail.com>
2021-09-18 18:47:20 +0530

    Birthday section added

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,16 @@
 import FBBox from "./FBBox";
 
 const Feed = () => {
-    return <div className="col-3">
-        <FBBox>Birthdays</FBBox>
+    return <div className="col-3 Feed">
+        <FBBox>
+            <div className="BirthdayContainer">
+                <img src="https://cdn-icons-png.flaticon.com/512/1139/1139982.png" alt="Gift" />
+                    <a href="/">Hemanth</a>, <a href="/">Mark Zuck</a>, <a href="/">Sundar Pichai </a>{" "}
+                    and 3 others.
+                
+            </div>
+
+        </FBBox>
         <FBBox>Pages</FBBox>
         <FBBox>Feed</FBBox>
         </div>;
